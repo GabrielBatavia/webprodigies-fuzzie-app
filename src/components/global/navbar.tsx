46bdf9266a9264b370ada64d8e58a053a6727c40
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-10-26 14:12:48 +0700

    feat: setup and main feature implementation - Add Navbar component with menu items - Update font to DM_Sans for global text styling - Add radial gradient background to main section - Add centered CTA button with hover effects - Implement responsive ContainerScroll with animation using Framer Motion

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -2,6 +2,7 @@ import { headers } from "next/headers";
 import Image from 'next/image';
 import React from "react";
 import Link from "next/link";
+import { MenuIcon } from 'lucide-react'
 
 type Props = {}
 
@@ -41,7 +42,24 @@ const Navbar = async (props: Props) => {
                 </li>
                 </ul>
             </nav>
+            <aside className="flex items-center gap-4">
+            <Link href="/dashboard"
+            className="relative inline-flex h-10 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+            >
+            <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#2EC8FF_0%,#393B82_50%,#2EC8FF_100%)]"></span>
+            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+                {//WIP : wire up user
+                true ? 'Dashboard' : 'Get Started'}
+            </span>
+            </Link>
+            {
+                //WIP : wire up user
+            }
+            <MenuIcon className="md:hidden"/>
+            </aside>
         </header>
+
+        
       );
     };
 export default Navbar
